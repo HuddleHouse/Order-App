4b24c07931868368325d7108f352d4a505e4b6d2
Matt Huddleston <mthuddleston@gmail.com>
2016-03-05 22:53:48 -0500

    Added checks on registration.

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -118,8 +118,11 @@ class AdminController extends Controller
      */
     public function viewAllUsersAction(Request $request)
     {
-        $conn = $this->get('database_connection');
-        $users = $conn->fetchAll("select CONCAT_WS(' ', first_name, last_name) as name, email, id from users");
+        // $conn = $this->get('database_connection');
+        // $users = $conn->fetchAll("select CONCAT_WS(' ', first_name, last_name) as name, email, id from users");
+
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository('AppBundle:User')->findAll();
 
         return $this->render('@App/Admin/view_users.html.twig', array(
             'users' => $users
