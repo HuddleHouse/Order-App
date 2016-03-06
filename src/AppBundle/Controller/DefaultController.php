3e097c93f391a03995f921f5361fa4c4d383e6fd
Matt Huddleston <mthuddleston@gmail.com>
2016-03-06 17:53:42 -0500

    Adding Datatables functionality

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,15 +2,9 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Invitation;
-use AppBundle\Form\DataTransformer\InvitationToCodeTransformer;
-use AppBundle\Form\InvitationFormType;
 use AppBundle\Form\InvitationType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use FOS\UserBundle\Event\GetResponseUserEvent;
-use Symfony\Component\Form\FormEvent;
 
 
 
@@ -29,6 +23,10 @@ class DefaultController extends Controller
      */
     public function userHomeAction()
     {
-        return $this->render('AppBundle:User:home.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $products = $em->getRepository('AppBundle:Part')->findAll();
+        return $this->render('AppBundle:User:home.html.twig', array(
+            'products' => $products
+        ));
     }
 }
