e59894f189f19b46efb065c5830bada5980d158c
Matt Huddleston <mthuddleston@gmail.com>
2016-03-10 18:11:36 -0500

    Finishing up the API some

--- src/AppBundle/Controller/CartController.php
+++ src/AppBundle/Controller/CartController.php
@@ -21,4 +21,19 @@ class CartController extends Controller
             'categories' => $categories,
         ));
     }
+
+    /**
+     * @Route("/review-order", name="review_order")
+     */
+    public function reviewOrderAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $products = $em->getRepository('AppBundle:Part')->findAll();
+        $categories = $em->getRepository('AppBundle:PartCategory')->findAll();
+
+        return $this->render('AppBundle:User:home.html.twig', array(
+            'products' => $products,
+            'categories' => $categories,
+        ));
+    }
 }
