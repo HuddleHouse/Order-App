82b611c54b6294e6e0ea5760bbdf893c26336bd8
Matt Huddleston <mthuddleston@gmail.com>
2016-03-19 17:27:49 -0400

    Cleaning up the code some.

--- src/AppBundle/Services/BaseService.php
+++ src/AppBundle/Services/BaseService.php
@@ -2,11 +2,9 @@
 
 namespace AppBundle\Services;
 
-use Symfony\Component\DependencyInjection\ContainerInterface as Container;
 
 class BaseService
 {
-    protected $container;
     protected $mailer;
 
     public function __construct(\Swift_Mailer $mailer)
