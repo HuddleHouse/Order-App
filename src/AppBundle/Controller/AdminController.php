97f669c0cac9ba5cd66166dabcece25e3e1a26de
Matt Huddleston <mthuddleston@gmail.com>
2016-03-22 09:33:02 -0400

    Cleaning up the code some.

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -268,7 +268,8 @@ class AdminController extends Controller
          * SEND EMAILS TO EVERYONE HERE
          *
          */
-        
+
+
         return $this->render('AppBundle:Admin:approve_order.html.twig', array(
             'products' => $products,
             'categories' => $categories,
