dcf52b73e02cc011b733f54b2c8edfb84d2bc073
Matt Huddleston <mthuddleston@gmail.com>
2016-05-14 18:25:52 -0400

    Started adding the add an unknown item option on the ordering page.

--- web/app_dev.php
+++ web/app_dev.php
@@ -23,6 +23,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
  */
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
+umask(0002);
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
