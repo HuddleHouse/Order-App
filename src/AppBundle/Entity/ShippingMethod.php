05aaabfb45879a89f2a321c03b949f48fedfa699
Matt Huddleston <mthuddleston@gmail.com>
2016-05-14 18:20:07 -0400

    Started adding the add an unknown item option on the ordering page.

--- src/AppBundle/Entity/ShippingMethod.php
+++ src/AppBundle/Entity/ShippingMethod.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\Mapping as ORM;
  * ShippingMethod
  *
  * @ORM\Table(name="shipping_method")
- * @ORM\Entity(repositoryClass="AppBundle\Repository\ShippingMethodRepository")
  */
 class ShippingMethod
 {
