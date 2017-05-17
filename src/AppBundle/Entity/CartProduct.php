b7a2ddb2e912fca04464dcabfa5cc4af2dbf6565
Matt Huddleston <mthuddleston@gmail.com>
2017-05-16 23:22:03 -0400

    #30 #31 #32

--- src/AppBundle/Entity/CartProduct.php
+++ src/AppBundle/Entity/CartProduct.php
@@ -372,7 +372,7 @@ class CartProduct
 
     public function getStockNumber()
     {
-        return $this->stockNumber ? $this->stockNumber : $this->getPart()->getStockNumber() ? $this->getPart()->getStockNumber() : '';
+        return $this->stockNumber;
     }
 
     public function setStockNumber($stockNumber)
