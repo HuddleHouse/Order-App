afab1270d2296861702494b3e8c13937c3335b6e
Filip Petkovski <filip.petkovsky@gmail.com>
2018-04-05 20:48:51 +0200

    Modify ordered parts db table

--- src/AppBundle/Controller/Api/AdminController.php
+++ src/AppBundle/Controller/Api/AdminController.php
@@ -20,7 +20,7 @@ class AdminController extends Controller
                 p.returned_items_quantity as returnedItemsQuantity,
                 p.returned_items_shipped_quantity as returnedItemsShippedQuantity,
                 parts.require_return as requireReturn, p.back_order_quantity as backOrderQuantity,
-                c.order_number as orderNumber, c.submit_date as submitDate, c.approve_date as approveDate,
+                c.id as cartId, c.order_number as orderNumber, c.submit_date as submitDate, c.approve_date as approveDate,
                 CONCAT_WS(\" \", c.requester_first_name, c.requester_last_name) as submittedBy,
                 CONCAT_WS(\" \", u2.first_name, u2.last_name) as approvedBy, o.name as officeName,
                 COALESCE (parts.stock_number, p.stock_number) as stockNumber,
@@ -54,9 +54,12 @@ class AdminController extends Controller
     {
         $products = $this->orderedPartsDbQuery();
 
-        $productsArray = array_map(function($product) {
+        $productsArray = array_map(function($product, $idx) {
+            $orderRoute = $this->generateUrl('admin_order_approve', [
+                'cart_id' => $product['cartId']
+            ]);
             return [
-                $product['catName'] ?: "UNLISTED ITEMS",
+                $idx + 1,
                 $product['stockNumber'],
                 $product['orderNumber'],
                 $product['submitDate'],
@@ -66,13 +69,11 @@ class AdminController extends Controller
                 $product['requireReturn'] ? "<center><i class='fa fa-check-circle-o' style='font-size: 25px;color: #e2001a;'></i></center>" : '',
                 $product['quantity'],
                 $product['shipQuantity'],
-                $product['returnedItemsQuantity'],
                 $product['backOrderQuantity'],
-                $product['submittedBy'],
-                $product['approvedBy'],
-                $product['officeName']
+                $product['officeName'],
+                '<a href="'.$orderRoute.'" class="btn btn-default btn-xs">View</a>'
             ];
-        }, $products);
+        }, $products, array_keys($products));
 
         return new JsonResponse([
             'data' => $productsArray
